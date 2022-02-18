ae1b20b02853e0a4fe07b63749842181f8aa84f7
Alexisfco10 <89372142+Alexisfco10@users.noreply.github.com>
2022-02-17 23:26:07 -0400

    Initial commit

---
+++ Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_shallow
+{
+    public class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+        public String Nombre { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Prototype_shallow;
+
+namespace Prototype
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal() { Patas=4, Nombre="Perro" };
+            Animal animalClon = animal1.Clone() as Animal;
+            animalClon.Nombre = "Gato";
+
+            Console.WriteLine(animalClon.Nombre);            
+            Console.WriteLine(animalClon.Patas);
+            Console.ReadKey();
+        }
+
+    }
+
+}
